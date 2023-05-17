54c28eeaf4d9ee5815192174bf929dd8b451f444
Lasantha <lasantha1130@gmail.com>
2023-05-17 14:08:46 +0530

    optimize code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Header from "./header";
 import Intro from "./Intro";
+import Projects from "./projects";
+import Contact from "./Contact";
 import Footer from "./Footer";
-
-
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Projects from "./projects";
-import Contact from "./Contact";
 
 function App() {
   return (
