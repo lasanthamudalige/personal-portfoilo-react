bc6cef33c97ce08ea73eab591beef38e2f6c83b7
Lasantha <lasantha1130@gmail.com>
2023-06-04 21:14:07 +0530

    show technologies used for each project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,39 +2,45 @@ const projects = [
     {
         id: 1,
         title: "Keeper app",
+        // Add require to fix image not showing
         imgUrl: require("../images/keeper_app.png"),
         link: "https://github.com/lasanthamudalige/keeper-app",
-        technologies: ["React", "a", "b"]
+        technologies: ["React"]
     },
     {
         id: 2,
         title: "Wiki API",
         imgUrl: require("../images/wiki_api.png"),
-        link: "https://github.com/lasanthamudalige/wiki-api"
+        link: "https://github.com/lasanthamudalige/wiki-api",
+        technologies: ["Express", "MongoDB"]
     },
     {
         id: 3,
         title: "TO Do list",
         imgUrl: require("../images/todo_list.png"),
-        link: "https://github.com/lasanthamudalige/todo-list-node"
+        link: "https://github.com/lasanthamudalige/todo-list-node",
+        technologies: ["Express", "MongoDB"]
     },
     {
         id: 4,
         title: "Personal blog",
         imgUrl: require("../images/personal_blog.png"),
-        link: "https://github.com/lasanthamudalige/personal-blog-node"
+        link: "https://github.com/lasanthamudalige/personal-blog-node",
+        technologies: ["Express", "MongoDB", "Bootstrap"]
     },
     {
         id: 5,
         title: "Developer portfolio",
         imgUrl: require("../images/developer_portfolio.png"),
-        link: "https://github.com/lasanthamudalige/personal-portfoilo-react"
+        link: "https://github.com/lasanthamudalige/personal-portfoilo-react",
+        technologies: ["React", "Bootstrap"]
     },
     {
         id: 6,
         title: "API based website",
         imgUrl: require("../images/api_based_dictionary.png"),
-        link: "https://github.com/lasanthamudalige/api-based-dictionary"
+        link: "https://github.com/lasanthamudalige/api-based-dictionary",
+        technologies: ["Flask"]
     }
 ];
 
